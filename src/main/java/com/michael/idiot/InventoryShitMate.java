2e771d4e1ec36d91236116c402bb107599a60e1d
Mookelol <mookeispro@gmail.com>
2017-07-27 05:03:11 +0100

    Absoluetly fucking nothing, I'm feeding off Ryan hehe :3

---
+++ src/main/java/com/michael/idiot/InventoryShitMate.java
@@ -0,0 +1,13 @@
+package com.michael.idiot;
+
+import com.michael.idiot.events.InventoryHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InventoryShitMate extends JavaPlugin {
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new InventoryHandler(), this);
+    }
+
+    public void onDisable() {
+    }
+}
