69a891561d20907ae2e6faee373a789016693dab
Nayrisian <nayrisian@gmail.com>
2017-08-05 23:25:11 +0100

    Updated inventory handler. {Unfinished}

--- src/main/java/com/michael/idiot/inventoryshitmate/Ref.java
+++ src/main/java/com/michael/idiot/inventoryshitmate/Ref.java
@@ -6,5 +6,9 @@ package com.michael.idiot.inventoryshitmate;
 @SuppressWarnings("unused WeakerAccess")
 public class Ref {
     public static final String PLUGIN_ID = "shitinv";
-    public static final String PERM_INVENTORY_BYPASS = PLUGIN_ID + ".inventory.bypass";
+    public static final String PERM_INVENTORY_BYPASS = PLUGIN_ID + ".bypass.inventory";
+    public static final String PERM_HELMET_ACCESS = PLUGIN_ID + ".access.helmet";
+    public static final String PERM_CHESTPLATE_ACCESS = PLUGIN_ID + ".access.chestplate";
+    public static final String PERM_LEGGINGS_ACCESS = PLUGIN_ID + ".access.leggings";
+    public static final String PERM_BOOTS_ACCESS = PLUGIN_ID + ".access.boots";
 }
